1dd8e8f75ad4f081bf7c2e1147cb78f7344605f3
ktomecki <tomecki.krzysiek@gmail.com>
2020-04-15 18:53:58 +0200

    Updates

---
+++ precache-manifest.3dd4c4f2a47c72808b3c61d8a8721d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1fa3f16a57392ab8b90f9bf8326865",
+    "url": "/custom-item-select/index.html"
+  },
+  {
+    "revision": "d9beff3dcd19a1a1d9fe",
+    "url": "/custom-item-select/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "7d7bbbd0295734e789d9",
+    "url": "/custom-item-select/static/css/main.200b577c.chunk.css"
+  },
+  {
+    "revision": "d9beff3dcd19a1a1d9fe",
+    "url": "/custom-item-select/static/js/2.c522fe1b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/custom-item-select/static/js/2.c522fe1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7bbbd0295734e789d9",
+    "url": "/custom-item-select/static/js/main.cf898787.chunk.js"
+  },
+  {
+    "revision": "cd138a02588712e5dbca",
+    "url": "/custom-item-select/static/js/runtime-main.0694c317.js"
+  }
+]);
