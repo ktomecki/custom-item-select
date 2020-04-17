29a9d7d981822e437d81def51b59f6a6fe0ab4e3
ktomecki <tomecki.krzysiek@gmail.com>
2020-04-17 11:37:54 +0200

    Updates

---
+++ precache-manifest.25fdf33a1967ed3aa0f0b1b404a3b9fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79ca07e79abbe38b5af320b8e7c9457",
+    "url": "/custom-item-select/index.html"
+  },
+  {
+    "revision": "ae84e4a3ae0c53a4f153",
+    "url": "/custom-item-select/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "6b71098a5d507715300a",
+    "url": "/custom-item-select/static/css/main.349ea464.chunk.css"
+  },
+  {
+    "revision": "ae84e4a3ae0c53a4f153",
+    "url": "/custom-item-select/static/js/2.3a70a070.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/custom-item-select/static/js/2.3a70a070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b71098a5d507715300a",
+    "url": "/custom-item-select/static/js/main.a0353cba.chunk.js"
+  },
+  {
+    "revision": "cd138a02588712e5dbca",
+    "url": "/custom-item-select/static/js/runtime-main.0694c317.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/custom-item-select/precache-manifest.3dd4c4f2a47c72808b3c61d8a8721d92.js"
+  "/custom-item-select/precache-manifest.25fdf33a1967ed3aa0f0b1b404a3b9fa.js"
 );
 
 self.addEventListener('message', (event) => {
